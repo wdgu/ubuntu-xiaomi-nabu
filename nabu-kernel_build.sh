cbb53a106a2e0600e1c7a7f614442200b15208f7
wdgu <101711528+wdgu@users.noreply.github.com>
2025-02-14 15:31:02 +0800

    Update nabu-kernel_build.sh

--- nabu-kernel_build.sh
+++ nabu-kernel_build.sh
@@ -5,6 +5,8 @@ cd linux
 make -j$(nproc) ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- defconfig sm8150.config
 make -j$(nproc) ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu-
 _kernel_version="$(make kernelrelease -s)"
+# 将内核版本号保存到文件
+echo "$_kernel_version" > ../kernel_version.txt
 mkdir ../linux-xiaomi-nabu/boot
 cp arch/arm64/boot/Image.gz ../linux-xiaomi-nabu/boot/vmlinuz-$_kernel_version
 cp arch/arm64/boot/dts/qcom/sm8150-xiaomi-nabu.dtb ../linux-xiaomi-nabu/boot/dtb-$_kernel_version
@@ -13,8 +15,8 @@ rm -rf ../linux-xiaomi-nabu/lib
 make -j$(nproc) ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- INSTALL_MOD_PATH=../linux-xiaomi-nabu modules_install
 rm ../linux-xiaomi-nabu/lib/modules/**/build
 cd ..
-#rm -rf linux
+rm -rf linux
 
 dpkg-deb --build --root-owner-group linux-xiaomi-nabu
 dpkg-deb --build --root-owner-group firmware-xiaomi-nabu
-dpkg-deb --build --root-owner-group alsa-xiaomi-nabu
+dpkg-deb --build --root-owner-group alsa-xiaomi-nabu 
